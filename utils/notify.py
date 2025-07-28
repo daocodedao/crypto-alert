d1a15db08465f1fd4a0cae166646f4d8a91ae708
linzhiji <linzhiji@ishehui.com>
2025-07-28 16:07:01 +0800

    插入数据库

--- utils/notify.py
+++ utils/notify.py
@@ -1,5 +1,7 @@
 
 
+import sys,os
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 from utils.curl import Curl
 from utils.util import Util
 
@@ -7,7 +9,7 @@ from utils.util import Util
 class NotifyUtil:
     @staticmethod
     def notifyFeishu(msg:str):
-        notifyUrl = "https://open.feishu.cn/open-apis/bot/v2/hook/6a98b9a1-650c-4b80-95d9-314fed9113efc"
+        notifyUrl = "https://open.feishu.cn/open-apis/bot/v2/hook/6a98b9a1-650c-4b80-95d9-314fed9113ef"
         para = {
             "msg_type":"text",
             "content":{
@@ -18,4 +20,5 @@ class NotifyUtil:
 
 
 
-
+if __name__ == "__main__":
+    NotifyUtil.notifyFeishu("测试")
