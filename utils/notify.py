1505d56cc20a0588a6cbe5f3dd6a03b5fb334b31
linzhiji <linzhiji@ishehui.com>
2025-07-28 16:04:18 +0800

    插入数据库

--- utils/notify.py
+++ utils/notify.py
@@ -7,7 +7,7 @@ from utils.util import Util
 class NotifyUtil:
     @staticmethod
     def notifyFeishu(msg:str):
-        notifyUrl = "https://open.feishu.cn/open-apis/bot/v2/hook/5550de0e-bf8e-423c-9495-9fbf929d457c"
+        notifyUrl = "https://open.feishu.cn/open-apis/bot/v2/hook/6a98b9a1-650c-4b80-95d9-314fed9113efc"
         para = {
             "msg_type":"text",
             "content":{
